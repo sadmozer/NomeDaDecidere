7ad33d0b44004d593a2d51ef88308ca4254a06cd
NICCOLO' CARDELLI <n.cardelli2@studenti.unipi.it>
2019-03-05 18:16:41 +0100

    Sistemato il codice

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const ARROW_UP = 38;
 const ARROW_RIGHT = 39;
 const ARROW_DOWN = 40;
 
-const diagMagn = Math.cos(Math.PI / 4);
+const diagMagn = Math.cos(Math.PI / 4) * 0.875;
 const Versor = [
     [diagMagn, 1, diagMagn],
     [1, 0, 1],
@@ -54,13 +54,13 @@ function Animator(src, mirrorsrc, frames, width, height) {
 Animator.prototype = {
     nextImageIndex: function () {
         if(this.orientation) {
-            return (Math.trunc(x++/4)) % 8;
+            return (Math.trunc(this.x++/4)) % 8;
         }
         else {
-            if(Math.trunc(y/4) >= 8) {
-                y -= 8*4;
+            if(Math.trunc(this.y/4) >= 8) {
+                this.y -= 8*4;
             }
-            return (8-Math.trunc(y++/4)) % 8
+            return (8-Math.trunc(this.y++/4)) % 8
         }
     }
 }
@@ -84,7 +84,7 @@ GameObject.prototype = {
     }
 }
 
-
+var flag = false;
 function KeyDownManager(event) {
     event.preventDefault();
     if(event.keyCode === ARROW_LEFT) {
@@ -103,8 +103,9 @@ function KeyDownManager(event) {
         Keys.ArrowDown = true;
         Axis.Vertical = 1;
     }
-    if(event.keyCode === 32) {
+    if(event.keyCode === 32 && !event.repeat) {
         Keys.Space = true;
+        flag = true;
     }
 }
 
@@ -138,8 +139,9 @@ function KeyUpManager(event) {
         else
             Axis.Vertical = -1;
     }
-    if(event.keyCode === 32) {
+    if(event.keyCode === 32 && !event.repeat) {
         Keys.Space = false;
+        console.log("yee");
     }
 }
 
@@ -147,33 +149,34 @@ document.addEventListener('keydown', KeyDownManager, false);
 document.addEventListener('keyup', KeyUpManager, false);
 
 var player;
-var x = 0;
-var y = 0;
 var background;
 const speed = 4.5;
 var worldMovement;
-
+var collected;
 function Start() {
-    background = new GameObject('Background', new Vector2(0, 0), new Renderer("Grass2.png", null));
-    
+    background = new GameObject('Background', new Vector2(0, 0), new Renderer("Grass2.png"));
+    bucket = new GameObject("Bucket", new Vector2(100, 100), new Renderer("Bucket-Idle.png"));
     player = new GameObject("Player", new Vector2(150, 150));
     player.addComponent("Renderer", new Renderer("Frog_Idle_COLORv1.png", "Frog_Idle_COLORv1 - Flipped.png"));
     player.addComponent("Animator", new Animator("Frog_Run_COLORv1.png", "Frog_Run_COLORv1 - Flipped.png", 8, 64, 64));
-
-    goat = new GameObject("Goat", new Vector2(100, 100), new Renderer("Bucket-Idle.png"));
     
+    bucketCenter = new Vector2(100 + bucket.Renderer.image.width/2, 100 + bucket.Renderer.image.height/2);
+    playerCenter = new Vector2(150 + player.Renderer.image.width/2, 150 + player.Renderer.image.height/2)
+
     emptyCanvas = context.createImageData(canvas.width, canvas.height);
     for (var i = 0; i < emptyCanvas.data.lenght; i++) {
         emptyCanvas.data[i] = 0;
     }
 
     GameObjectList.push(background);
-    GameObjectList.push(goat);
+    GameObjectList.push(bucket);
+    collected = false;
 }
 
 function Update(deltaTime) {
     worldMovement = new Vector2(Math.trunc(Axis.Horizontal * Versor[Axis.Horizontal + 1][Axis.Vertical + 1] * speed ), Math.trunc(Axis.Vertical * Versor[Axis.Horizontal + 1][Axis.Vertical + 1] * speed)); 
 
+    console.log(worldMovement);
     player.Transform.x += worldMovement.x;
     player.Transform.y += worldMovement.y;
     
@@ -184,6 +187,27 @@ function Update(deltaTime) {
     else if ((!player.Animator.orientation && Axis.Horizontal == 1)){
         player.Animator.orientation = true;
         player.Renderer.orientation = true;
+    }
+
+    
+    bucketCenter.x = bucket.Transform.x + bucket.Renderer.image.width/2;
+    bucketCenter.y = bucket.Transform.y + bucket.Renderer.image.height/2;
+
+    playerCenter.x = player.Transform.x + player.Renderer.image.width/2;
+    playerCenter.y = player.Transform.y + player.Renderer.image.height/2
+
+    if(flag) {
+        if(!collected && Math.hypot(bucketCenter.x-playerCenter.x, bucketCenter.y-playerCenter.y) < 30) {
+            collected = true;
+        }
+        else if(collected) {
+            collected = false;
+        }
+        flag = false;
+    }
+    if(collected) {
+        bucket.Transform.x = player.Transform.x;
+        bucket.Transform.y = player.Transform.y;
     }
 }
 function Render() {
@@ -209,6 +233,9 @@ function Render() {
             context.drawImage(player.Renderer.mirrorImage, player.Transform.x, player.Transform.y);
         }
     }
+    context.beginPath();
+    context.ellipse(playerCenter.x, playerCenter.y, 32, 32, 0, 0, 2 * Math.PI);
+    context.stroke();
 }
 
 var lastTime = 0;
