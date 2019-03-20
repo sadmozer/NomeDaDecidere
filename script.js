2086713e636cb6f8a0e5e22189ddd02363204463
NICCOLO' CARDELLI <n.cardelli2@studenti.unipi.it>
2019-03-20 01:23:17 +0100

    Aggiunta curva Bezier lancio, modificata animazione Player

--- script.js
+++ script.js
@@ -22,13 +22,16 @@ const ARROW_DOWN = 83;
 
 const diagMagn = Math.cos(Math.PI / 4);
 // Beatty sequence number
-const speed = 7;
+const speed = 6;
+const flySpeed = 30;
+const arcHeight = 30;
 const s = speed / (Math.sqrt(2)*Math.trunc(speed/Math.sqrt(2)))
 const Versor = [
     [diagMagn, 1, diagMagn],
     [1, 0, 1],
     [diagMagn, 1, diagMagn]
 ]
+const rect = canvas.getBoundingClientRect();
 
 function Vector2 (x, y) {
     this.x = x;
@@ -158,7 +161,6 @@ function KeyUpManager(event) {
     }
 }
 
-var rect = canvas.getBoundingClientRect();
 function MouseDownManager(event) {
     beginLine.x = Math.trunc(event.clientX-rect.left);
     beginLine.y = Math.trunc(event.clientY-rect.top);
@@ -232,12 +234,12 @@ var endLine;
 var draw;
 var player;
 var background;
-var target;
 var deltaWorldMovement;
 var worldMovement;
 var collected;
 var CollectibleFactory;
 var secchio2;
+var vect;
 function Start() {
     CollectibleFactory = new GameObjectFactory();
 
@@ -249,25 +251,30 @@ function Start() {
         spawn: new Vector2(30, 30)
     });
     
+    bucket = CollectibleFactory.create({
+        goClass: "Collectible",
+        name: "bucket",
+        state: "Idle",
+        renderer: new Renderer("Bucket-Idle.png"),
+        spawn: new Vector2(100, 100)
+    });
+    
     beginLine = new Vector2(0, 0);
     endLine = new Vector2(0, 0);
     draw = false;
     spawn = new Vector2(150, 150);
-    target = new GameObject("Target", new Vector2(0, 0), new Renderer("Target1.png"));
+    vect = new Vector2(0, 0);
     background = new GameObject('Background', new Vector2(0, 0), new Renderer("Grass2.png"));
-    bucket = new GameObject("Bucket", new Vector2(100, 100), new Renderer("Bucket-Idle.png"));
     player = new GameObject("Player", new Vector2(spawn.x, spawn.y));
     player.addComponent("Renderer", new Renderer("Frog_Idle_COLORv1.png", "Frog_Idle_COLORv1 - Flipped.png"));
     player.addComponent("Animator", new Animator("Frog_Run_COLORv1.png", "Frog_Run_COLORv1 - Flipped.png", 8, 64, 64));
     
-    bucketCenter = new Vector2(100 + bucket.Renderer.image.width/2, 100 + bucket.Renderer.image.height/2);
     playerCenter = new Vector2(150 + player.Renderer.image.width/2, 150 + player.Renderer.image.height/2)
     
     emptyCanvas = context.createImageData(canvas.width, canvas.height);
     for (var i = 0; i < emptyCanvas.data.lenght; i++) {
         emptyCanvas.data[i] = 0;
     }
-    bucket.setState("Idle");
     GameObjectList.push(background);
     GameObjectList.push(bucket);
     GameObjectList.push(secchio2);
@@ -276,6 +283,9 @@ function Start() {
 }
 var historyMovement;
 var k = 1;
+var flyStart;
+var flyControl;
+var flyEnd;
 function Update(deltaTime) {
     deltaWorldMovement.x = Math.trunc(Axis.Horizontal * Versor[Axis.Horizontal + 1][Axis.Vertical + 1] * speed);
     deltaWorldMovement.y = Math.trunc(Axis.Vertical * Versor[Axis.Horizontal + 1][Axis.Vertical + 1] * speed); 
@@ -284,21 +294,18 @@ function Update(deltaTime) {
         worldMovement.x += deltaWorldMovement.x;
         worldMovement.y += deltaWorldMovement.y;
     }
-
+    
     player.Transform.x += deltaWorldMovement.x;
     player.Transform.y += deltaWorldMovement.y;
     
-    target.Transform.x = Math.trunc(mouseX)+player.Transform.x-spawn.x;
-    target.Transform.y = Math.trunc(mouseY)+player.Transform.y-spawn.y;
-    
-    bucketCenter.x = bucket.Transform.x + bucket.Renderer.image.width/2;
-    bucketCenter.y = bucket.Transform.y + bucket.Renderer.image.height/2;
-    
     playerCenter.x = player.Transform.x + player.Renderer.image.width/2;
     playerCenter.y = player.Transform.y + player.Renderer.image.height/2
-
-    player.Animator.orientation = (mouseX >= spawn.x + player.Renderer.image.width/2);
-    // console.log(bucket.getState());
+    
+    if(draw)
+    player.Animator.orientation = (beginLine.x >= spawn.x + player.Renderer.image.width/2);
+    else
+        player.Animator.orientation = (mouseX >= spawn.x + player.Renderer.image.width/2);
+    
     var CollectibleList = [secchio2, bucket];
     CollectibleList.forEach(function(element) {
     
@@ -310,15 +317,18 @@ function Update(deltaTime) {
             }
             else {
                 element.setState("Idle");
-                // if(flag)
-                //     flag = false;
             }
             break;
         case "Trasporto":
             if(flag && Math.sqrt(Math.pow(beginLine.x-endLine.x, 2) + Math.pow(beginLine.y-endLine.y, 2))>20) {
                 element.setState("In volo");
                 k = 1;
-                flag = false;
+                vect.x = beginLine.x-endLine.x;
+                vect.y = beginLine.y-endLine.y;
+                flyStart = new Vector2(player.Transform.x+deltaWorldMovement.x, player.Transform.y+deltaWorldMovement.y);
+                flyControl = new Vector2(vect.x/2 + player.Transform.x, vect.y/2/1.2 + player.Transform.y-arcHeight);
+                flyEnd = new Vector2(vect.x + player.Transform.x, vect.y/1.2 + player.Transform.y + 30);
+                console.log(flyStart, flyControl, flyEnd);
             }
             else {
                 element.Transform.x = player.Transform.x;
@@ -326,9 +336,10 @@ function Update(deltaTime) {
             }
             break;
         case "In volo":
-            if(k < 20){
-                element.Transform.x = (Math.trunc(k*(beginLine.x - endLine.x)/20) + player.Transform.x);
-                element.Transform.y = (Math.trunc(k*(beginLine.y - endLine.y)/20) + player.Transform.y);
+            if(k < flySpeed){
+                var fly = Bezier3(flyStart, flyControl, flyEnd, k/flySpeed);
+                element.Transform.x = fly.x;
+                element.Transform.y = fly.y;
                 k++;
                 element.setState("In volo");
             }
@@ -341,6 +352,9 @@ function Update(deltaTime) {
             break;
     }
     });
+    if(flag)
+        flag = false;
+    // console.log(deltaTime) ;
 }
 function Render() {
     context.translate(-deltaWorldMovement.x, -deltaWorldMovement.y);
@@ -350,11 +364,25 @@ function Render() {
         context.drawImage(currObj.Renderer.image, currObj.Transform.x, currObj.Transform.y);
     }
     if(Axis.Horizontal || Axis.Vertical) {
-        if(player.Animator.orientation) {
-            context.drawImage(player.Animator.image, player.Animator.nextImageIndex() * 64, 0, 64, 64, player.Transform.x, player.Transform.y, 64, 64);
+        if(player.Animator.orientation && (Axis.Horizontal == 1 || Axis.Vertical)) {
+            context.drawImage(player.Animator.image, Math.trunc(player.Animator.x/4) % 8 * 64, 0, 64, 64, player.Transform.x, player.Transform.y, 64, 64);
+            player.Animator.x = (player.Animator.x + 1);
         }
-        else {
-            context.drawImage(player.Animator.mirrorImage, player.Animator.nextImageIndex() * 64, 0, 64, 64, player.Transform.x, player.Transform.y, 64, 64);
+        else if(!player.Animator.orientation && (Axis.Horizontal == 1 || Axis.Vertical)){
+            context.drawImage(player.Animator.mirrorImage, Math.trunc(player.Animator.y/4) % 8 * 64, 0, 64, 64, player.Transform.x, player.Transform.y, 64, 64);
+            player.Animator.y = (player.Animator.y + 1);
+        }
+        else if(!player.Animator.orientation && (Axis.Horizontal == -1)){
+            context.drawImage(player.Animator.mirrorImage, Math.trunc(player.Animator.y/4) % 8 * 64, 0, 64, 64, player.Transform.x, player.Transform.y, 64, 64);
+            player.Animator.y = (player.Animator.y - 1);
+            if(player.Animator.y < 0)
+                player.Animator.y+=8*4;
+        }
+        else if(player.Animator.orientation && (Axis.Horizontal == -1)){
+            context.drawImage(player.Animator.image, Math.trunc(player.Animator.x/4) % 8 * 64, 0, 64, 64, player.Transform.x, player.Transform.y, 64, 64);
+            player.Animator.x = (player.Animator.x - 1);
+            if(player.Animator.x < 0)
+                player.Animator.x+=8*4;
         }
     }
     else {
@@ -365,8 +393,16 @@ function Render() {
             context.drawImage(player.Renderer.mirrorImage, player.Transform.x, player.Transform.y);
         }
     }
+    // context.beginPath();
+    // context.ellipse(playerCenter.x, playerCenter.y, Math.abs(beginLine.x-mouseX), Math.abs(beginLine.y-mouseY)/2, 0, 0, 2 * Math.PI);
+    // context.stroke();
+
+    // context.beginPath();
+    // context.ellipse(playerCenter.x, playerCenter.y, Math.abs(beginLine.x-mouseX), Math.abs(beginLine.y-mouseY)/2, 0, 0, 2 * Math.PI);
+    // context.stroke();
+
     context.beginPath();
-    context.ellipse(playerCenter.x, playerCenter.y, 32, 32, 0, 0, 2 * Math.PI);
+    context.ellipse(playerCenter.x, playerCenter.y, 32, 32/1.2, 0, 0, 2 * Math.PI);
     context.stroke();
 
     if(draw) {
@@ -375,6 +411,12 @@ function Render() {
         context.lineTo(mouseX+player.Transform.x-spawn.x, mouseY+player.Transform.y-spawn.y);
         context.stroke();
     }
+}
+
+function Bezier3(start, control, end, t) {
+    var ret = new Vector2(Math.trunc(((1 - t)*(1 - t) * start.x) + (2 * t * (1 - t) * control.x) + (t * t * end.x)),
+    Math.trunc(((1 - t)*(1 - t) * start.y) + (2 * t * (1 - t) * control.y) + (t * t * end.y)));
+    return ret;
 }
 
 var lastTime = 0;
